5a8e055d580c3d76d2b55648df0eb73329ae79b2
Nolan Hamilton <57746191+nolan-h-hamilton@users.noreply.github.com>
2023-08-23 09:39:41 -0400

    update setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,17 +1,37 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name='rocco',
-    version='0.2.0',
+    version='0.3.2',
+    author='Nolan Holt Hamilton',
+    author_email='nolan.hamilton@unc.edu',
+    description='Robust ATAC-seq Peak Calling for Many Samples via Convex Optimization',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/nolan-h-hamilton/rocco',
     packages=find_packages(),
-    install_requires=['numpy==1.23.2','cvxpy==1.3.1',
-                      'pandas==1.5.3', 'pararead==0.7.0',
-                      'logmuse==0.2.7', 'scipy==1.10.1',
-                      'pysam==0.20.0', 'pybedtools==0.9.0',
-                      'ortools==9.3.10497'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords='peak-caller, atac-seq, consensus-peaks',
+    install_requires=[
+        'numpy',
+        'cvxpy',
+        'pandas',
+        'pararead',
+        'logmuse',
+        'scipy',
+        'pysam',
+        'pybedtools',
+        'ortools',
+    ],
     entry_points={
         'console_scripts': [
             'rocco = rocco.rocco:main'
         ]
-    }
+    },
 )
