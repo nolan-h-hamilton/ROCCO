9ff8a0711f7e425e593ccaba1c91c34fcde989d0
Nolan Hamilton <57746191+nolan-h-hamilton@users.noreply.github.com>
2023-11-16 17:25:46 -0500

    make all tests active

--- tests/test_rocco.py
+++ tests/test_rocco.py
@@ -101,30 +101,30 @@ def test_run_chrom_chr19():
     assert os.stat(chr19_outfile).st_size > peakfile_minsize
 
 
-# def test_run_chrom_chr20():
-#     """
-#     Ensure 'rocco chrom' runs successfully for chr20
-#     """
-#     # check if command returns exit code '0'
-#     assert subprocess.run(['rocco', 'chrom', '--chrom', 'chr20', '--wig_path', os.path.dirname(tracks_chrom20_wig),
-#                             '--budget', chr20_budget, '-N', rr_iter, '--start', chr20_start, '--end', chr20_end,  '--solver', solver],
-#                           capture_output=True, encoding='utf-8').returncode == 0,'rocco chrom returncode != 0'
-#     # check if output file was created properly
-#     assert chr20_outfile in os.listdir()
-#     # ensure output file is nonempty (at least 'x' bytes)
-#     assert os.stat(chr20_outfile).st_size > peakfile_minsize
+def test_run_chrom_chr20():
+    """
+    Ensure 'rocco chrom' runs successfully for chr20
+    """
+    # check if command returns exit code '0'
+    assert subprocess.run(['rocco', 'chrom', '--chrom', 'chr20', '--wig_path', os.path.dirname(tracks_chrom20_wig),
+                            '--budget', chr20_budget, '-N', rr_iter, '--start', chr20_start, '--end', chr20_end,  '--solver', solver],
+                          capture_output=True, encoding='utf-8').returncode == 0,'rocco chrom returncode != 0'
+    # check if output file was created properly
+    assert chr20_outfile in os.listdir()
+    # ensure output file is nonempty (at least 'x' bytes)
+    assert os.stat(chr20_outfile).st_size > peakfile_minsize
 
-# def test_run_gwide():
-#     # check if command returns exit code '0'
-#     assert subprocess.run(["rocco", "gwide", "-p", "test_params.csv","--outdir", gwide_outdir, "--combine", gwide_outfile, "-N", rr_iter,  '--solver', solver],
-#                           capture_output=True, encoding='utf-8').returncode == 0, 'rocco gwide returncode != 0'
-#     # check if output file was created properly
-#     assert gwide_outfile in os.listdir()
-#     # ensure output file is nonempty (at least 'x' bytes)
-#     assert os.stat(gwide_outfile).st_size > peakfile_minsize
-#     chrom_files = [x for x in os.listdir(gwide_outdir) if x.split('.')[-1] == 'bed']
-#     # ensure outdir contains chromosome-specific BED files
-#     assert len(chrom_files) == 2
+def test_run_gwide():
+    # check if command returns exit code '0'
+    assert subprocess.run(["rocco", "gwide", "-p", "test_params.csv","--outdir", gwide_outdir, "--combine", gwide_outfile, "-N", rr_iter,  '--solver', solver],
+                          capture_output=True, encoding='utf-8').returncode == 0, 'rocco gwide returncode != 0'
+    # check if output file was created properly
+    assert gwide_outfile in os.listdir()
+    # ensure output file is nonempty (at least 'x' bytes)
+    assert os.stat(gwide_outfile).st_size > peakfile_minsize
+    chrom_files = [x for x in os.listdir(gwide_outdir) if x.split('.')[-1] == 'bed']
+    # ensure outdir contains chromosome-specific BED files
+    assert len(chrom_files) == 2
 
 def test_comp_ref_chr19():
     """
@@ -137,24 +137,24 @@ def test_comp_ref_chr19():
     jaccard_chr19 = float(data.split("\t")[2])
     assert jaccard_chr19 >= jaccard_min
 
-# def test_comp_ref_chr20():
-#     """
-#     Ensure the generated BED files are reasonably consistent with constant references.
-#     Wont match perfectly due the random simulation, but should exhibit broad similarity
-#     """
-#     cmd = ["bedtools", "jaccard", "-a", chr20_outfile , "-b", chr20_ref_file ]
-#     proc = subprocess.run(cmd, capture_output=True, text=True)
-#     data = proc.stdout.splitlines()[1]
-#     jaccard_chr20 = float(data.split("\t")[2])
-#     assert jaccard_chr20 >= jaccard_min
+def test_comp_ref_chr20():
+    """
+    Ensure the generated BED files are reasonably consistent with constant references.
+    Wont match perfectly due the random simulation, but should exhibit broad similarity
+    """
+    cmd = ["bedtools", "jaccard", "-a", chr20_outfile , "-b", chr20_ref_file ]
+    proc = subprocess.run(cmd, capture_output=True, text=True)
+    data = proc.stdout.splitlines()[1]
+    jaccard_chr20 = float(data.split("\t")[2])
+    assert jaccard_chr20 >= jaccard_min
 
-# def test_comp_ref_gwide():
-#     """
-#     Ensure the generated BED files are reasonably consistent with constant references.
-#     Wont match perfectly due the random simulation, but should exhibit broad similarity
-#     """
-#     cmd = ["bedtools", "jaccard", "-a", gwide_outfile, "-b", gwide_ref_file]
-#     proc = subprocess.run(cmd, capture_output=True, text=True)
-#     data = proc.stdout.splitlines()[1]
-#     jaccard_gwide = float(data.split("\t")[2])
-#     assert jaccard_gwide >= jaccard_min
+def test_comp_ref_gwide():
+    """
+    Ensure the generated BED files are reasonably consistent with constant references.
+    Wont match perfectly due the random simulation, but should exhibit broad similarity
+    """
+    cmd = ["bedtools", "jaccard", "-a", gwide_outfile, "-b", gwide_ref_file]
+    proc = subprocess.run(cmd, capture_output=True, text=True)
+    data = proc.stdout.splitlines()[1]
+    jaccard_gwide = float(data.split("\t")[2])
+    assert jaccard_gwide >= jaccard_min
