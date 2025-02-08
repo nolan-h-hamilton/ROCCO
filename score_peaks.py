76c4c35d863fa1c13478289b02fffe6e1e703c3e
Nolan Hamilton <57746191+nolan-h-hamilton@users.noreply.github.com>
2025-02-08 13:29:33 -0500

    update score_peaks.py

--- score_peaks.py
+++ score_peaks.py
@@ -4,28 +4,28 @@ import rocco
 # import seaborn as sns # pip install seaborn
 
 BAM_LIST_FILE = 'bam_files.txt' # text file with paths to bam files (one per line)
-PEAK_FILE = 'test_peaks.bed' # ROCCO BED output (three columns)
+PEAK_FILE = 'test_peaks.bed' # ROCCO BED output 
 CHROM_SIZES_FILE = 'hg38.chrom.sizes' # sizes file
-PEAK_COUNT_MATRIX_FILE = 'peak_count_matrix.tsv' # output count matrix
+PEAK_COUNT_MATRIX_FILE = 'peak_count_matrix.tsv' # path to count matrix--if it doesn't exist already, it'll be generated
 OUTPUT_SCORED_PEAKS_FILE = 'rocco_peaks_scored.bed' # output scored peaks
 
-# If you already have the raw count matrix file, you can comment out the next block
-# I've not yet tested this with a large peak file. if it is slow, try `peak_count_matrix.sh'
+# I've not yet tested this with a large peak file. `peak_count_matrix.sh' may be faster
 print('\nGenerating raw count matrix...')
-count_matrix_path = rocco.readtracks.raw_count_matrix(bam_list_file=BAM_LIST_FILE,
-                                                                    peak_file=PEAK_FILE,
-                                                                    output_file=PEAK_COUNT_MATRIX_FILE)
-print('done.\n')
+if not os.path.exists(PEAK_COUNT_MATRIX_FILE):
+    PEAK_COUNT_MATRIX_FILE = rocco.readtracks.raw_count_matrix(bam_list_file=BAM_LIST_FILE,
+                                                                        peak_file=PEAK_FILE,
+                                                                        output_file=PEAK_COUNT_MATRIX_FILE)
+    print('done.\n')
 
 # just checking the above worked
-if not os.path.exists(count_matrix_path):
-    raise ValueError("Failed to create count matrix.")
+if not os.path.exists(PEAK_COUNT_MATRIX_FILE):
+    raise ValueError(f"Failed to find or create count matrix: {PEAK_COUNT_MATRIX_FILE}")
 
 
 # score peaks and store output in narrowPeak-like format
 # note that p-values and q-values are, per MACS convention, in -log10 scale
 # likewise, the bed6 scores are scaled in [0,1000] for browser compliance
-print(f'\nScoring peaks in {count_matrix_path}...')
+print(f'\nScoring peaks in {PEAK_COUNT_MATRIX_FILE}...')
 peak_scores, bed6_scores, pvals = rocco.readtracks.score_peaks(bam_files=BAM_LIST_FILE,
                                            chrom_sizes_file=CHROM_SIZES_FILE,
                                            peak_file=PEAK_FILE,
